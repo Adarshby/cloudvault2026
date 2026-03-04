60c9778bf8fe57d3639824e9d37585014b84a261
Adarsh B Y <adarsh773245@gmail.com>
2026-03-05 01:02:59 +0530

    Add share link feature

--- storage/models.py
+++ storage/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import uuid
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to='uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     download_count = models.IntegerField(default=0)
-
+    share_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    
 class AccessLog(models.Model):
     file = models.ForeignKey(File, on_delete=models.CASCADE)
     accessed_by = models.ForeignKey(User, on_delete=models.CASCADE)
