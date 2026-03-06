44273fad13159c787426a52089de3e12d3582d21
Adarsh B Y <adarsh773245@gmail.com>
2026-03-07 01:15:41 +0530

    Connect PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+import os
 from pathlib import Path
 import os
 
@@ -76,10 +78,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database (SQLite)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
