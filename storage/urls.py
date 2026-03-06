6e11d85ff9de9f8b68c221973e671193316ad1ae
Adarsh B Y <adarsh773245@gmail.com>
2026-03-07 02:11:28 +0530

    Fix admin routes

--- storage/urls.py
+++ storage/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
@@ -7,5 +9,9 @@ urlpatterns = [
     path('delete/<int:file_id>/', views.delete_file, name='delete_file'),
     path('register/', views.register, name='register'),
     path('share/<uuid:token>/', views.share_download, name='share_download'),
+    path('admin/', admin.site.urls),
+    path('', include('storage.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
 
