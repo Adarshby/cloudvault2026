306653ac91cf4c682983a9ff4d9df3afd1315d7a
Adarsh B Y <adarsh773245@gmail.com>
2026-03-07 22:51:18 +0530

    Fix Cloudinary download and share functionality

--- storage/views.py
+++ storage/views.py
@@ -1,17 +1,10 @@
-import os
-from django.shortcuts import render,redirect
+import requests
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.http import Http404
+from django.http import HttpResponse, Http404
+
 from .models import File, AccessLog
-import requests
-from django.http import HttpResponse
-import requests
-from django.http import HttpResponse
-
-
-
-
 
 
 # ----------------------------
@@ -23,18 +16,14 @@ def dashboard(request):
 
     files = File.objects.filter(user=request.user)
 
-    # Temporary storage calculation
+    # simple estimation (Cloudinary does not expose size easily)
     total_files = files.count()
-
-    # Assume avg 5MB per file (Cloudinary doesn't expose size easily)
     total_size = total_files * 5 * 1024 * 1024
 
     quota = 100 * 1024 * 1024
 
-    storage_used_percent = int((total_size / quota) * 100) if quota > 0 else 0
-
+    storage_used_percent = int((total_size / quota) * 100)
     used_mb = round(total_size / (1024 * 1024), 2)
-
     quota_mb = 100
 
     if request.method == "POST":
@@ -42,7 +31,6 @@ def dashboard(request):
         uploaded_file = request.FILES.get("file")
 
         if uploaded_file:
-
             File.objects.create(
                 user=request.user,
                 file=uploaded_file
@@ -65,16 +53,6 @@ def dashboard(request):
 # ----------------------------
 # Download file
 # ----------------------------
-
-
-
-
-
-
-import requests
-from django.http import HttpResponse, Http404
-from django.contrib.auth.decorators import login_required
-
 
 @login_required
 def download_file(request, file_id):
@@ -105,6 +83,7 @@ def download_file(request, file_id):
 
     return http_response
 
+
 # ----------------------------
 # Delete file
 # ----------------------------
@@ -113,11 +92,7 @@ def download_file(request, file_id):
 def delete_file(request, file_id):
 
     try:
-        file_obj = File.objects.get(
-            id=file_id,
-            user=request.user
-        )
-
+        file_obj = File.objects.get(id=file_id, user=request.user)
     except File.DoesNotExist:
         raise Http404("File not found")
 
@@ -143,21 +118,12 @@ def register(request):
     else:
         form = UserCreationForm()
 
-    return render(
-        request,
-        "registration/register.html",
-        {"form": form}
-    )
+    return render(request, "registration/register.html", {"form": form})
 
 
 # ----------------------------
 # Share download link
 # ----------------------------
-
-
-
-
-
 
 def share_download(request, token):
 
