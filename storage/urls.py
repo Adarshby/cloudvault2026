95670bcb9394e012c0b8dcf2ad2f5ea8d08a382d
Adarsh B Y <adarsh773245@gmail.com>
2026-03-07 17:04:03 +0530

    Setup PostgreSQL + Cloudinary storage

--- storage/urls.py
+++ storage/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
+
     path('', views.dashboard, name='dashboard'),
+
     path('download/<int:file_id>/', views.download_file, name='download_file'),
+
     path('delete/<int:file_id>/', views.delete_file, name='delete_file'),
-    path('register/', views.register, name='register'),
+
     path('share/<uuid:token>/', views.share_download, name='share_download'),
-    path('admin/', admin.site.urls),
-    path('', include('storage.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('register/', views.register, name='register'),
 ]
-
-
